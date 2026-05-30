e4627b3d23e58225f64788186e62328d18cd1cca
Yefer <yefersonnjar02192510@gmail.com>
2026-05-29 19:13:15 -0500

    Configuracion de rutas y documentacion Swagger

--- xchango_sistema/backen/urls.py
+++ xchango_sistema/backen/urls.py
@@ -16,6 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Xchango API Sistema",
+        default_version='sistema',
+        description="API REST CRUD para el módulo de administración del sistema Xchango.",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
