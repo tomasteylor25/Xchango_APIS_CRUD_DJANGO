affc45b23f5c45026f9de556c550ec0aab166c6a
Yefer <yefersonnjar02192510@gmail.com>
2026-05-29 19:30:50 -0500

    creacion de router de administración con registro de endpoints

--- API_Django_yeferson/backen/urls.py
+++ API_Django_yeferson/backen/urls.py
@@ -1,23 +1,21 @@
-"""
-URL configuration for backen project.
+from rest_framework.routers import DefaultRouter
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/6.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+from .views import (
+    NivelAccesoViewSet,
+    PermisoViewSet,
+    AdministradorViewSet,
+    AdministradorPermisoViewSet,
+    HistorialAdminViewSet,
+    NotificacionViewSet,
+)
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
-]
+#Registro de endpoints para cada api
+router = DefaultRouter()
+router.register(r'nivel_acceso',         NivelAccesoViewSet)
+router.register(r'permiso',              PermisoViewSet)
+router.register(r'administrador',        AdministradorViewSet)
+router.register(r'administradorpermiso', AdministradorPermisoViewSet)
+router.register(r'historialadmin',       HistorialAdminViewSet)
+router.register(r'notificacion',         NotificacionViewSet)
+
+urlpatterns = router.urls
