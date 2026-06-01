049922d9ac468d3ecaa74b28e8bb2f9d17e9cdb2
Yefer <yefersonnjar02192510@gmail.com>
2026-06-01 17:05:12 -0500

    prueba

--- xchango_sistema/run.py
+++ xchango_sistema/run.py
@@ -6,4 +6,4 @@ if __name__ == '__main__':
     from django.core.management import execute_from_command_line
     execute_from_command_line(['manage.py', 'runserver', '0.0.0.0:8081'])
 
-#prueba
+#prueba prueba
