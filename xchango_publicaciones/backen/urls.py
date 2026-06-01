0807c5be36286618f89bc9393c4b5bf0d95daf5d
MIGUEL200401 <miguelperez.di2004@gmail.com>
2026-06-01 14:47:18 -0500

    se hace cambios de v1 a publicaciones

--- xchango_publicaciones/backen/urls.py
+++ xchango_publicaciones/backen/urls.py
@@ -24,7 +24,7 @@ from drf_yasg import openapi
 schema_view = get_schema_view(
     openapi.Info(
         title="Xchango API Publicaciones",
-        default_version='v1',
+        default_version='publicaciones',
         description="API REST CRUD para el módulo de publicaciones del sistema Xchango.",
     ),
     public=True,
@@ -33,7 +33,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('v1/', include('api.urls')),
+    path('publicaciones/', include('api.urls')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
